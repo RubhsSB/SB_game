3203ce646a6d054c1bbfc1b1aa3280c81a1522b4
RubhsSB <rhs5656@gmail.com>
2025-01-27 16:38:03 +0100

    Update script.js
    Mejora palabras

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const words = [
  "DyY (onda + Círculo)", "SENSUAL +", "DyY (Giro + Freno en 6)", "70´Adelante", "JyE", "AyYUR Palanca", "Lado MAMBO", "GAB", "JyE Giro CUELLO", "DISOCIACION de PECHO (pos CERRADA)", "D e Inés", "INTRO", 
  "CyT (mano cuello LED en 8)", "Brazo Lanzo DEBAJO", "JLAB (Sensual/Giros)", "DOTORE (Mym)", "ARGETIN", "CANGURO", "PyL Pasos", "ENGAÑO", "CyC", "JyE", "GUITARRA", "SINCOPADO", "Pasos CARLOS", "STEPHAN y NEVI (freno en 3 y horiz)", 
  "SERGIO y KATINA (molino en 1 Casa Campo)", "EyG Giro Espalda con Espalda", "DyY (Giro en 8 + Lanzo izq)", "CyT (Balanceo)", "CyT (Péndulo)", "CyT (sensual meter pierna)", "CyT (Tumbar Follow)", "DyY (Cats Giro en 5 + Brazos)", 
- "IVAN Y SARAI (3 sensual + Cadera)", , "Diagonal - JHersey", "Diagonal - JHersey", "CyC (Giro en Diagonal + abrazo cuellos)"
+ "IVAN Y SARAI (3 sensual + Cadera)", "Diagonal - JHersey", "Diagonal - JHersey", "CyC (Giro en Diagonal + abrazo cuellos)"
 ];
 const wordsCustom = [
  "CyC (Host cambio posición)", "Daniel y Alma", "JyE (diagonal Cintura)", "EyG (abro y cierro)", "DyY (abro en 4)", "MARCO ESPEJO", "CyC (lanzo atrás en 3)","JHERSY - Preparar pos Cerrada", 
