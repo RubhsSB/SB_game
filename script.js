e3119eddf4e8681ef7fceceac1da167e1b8b8bef
RubhsSB <rhs5656@gmail.com>
2026-01-22 08:59:34 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const words = [
   "JHERSY (en sombra hacer Rueda)", "JHERSY (En posición cerrada dos Ondas laterales rápidas)", "JHERSY (Mambo - tensión brazos 1o Izq + 5º Decha)", "ByL (MAD - Lanzo Decha)", "MyS (MAD - Cambio pos en 1)",
   "Illusion", "Bailar en el sitio - Mov pequeños", "EyG (Break 1 Jowke - Preparo en 4, en 5 estiro atrás Drcho)", "EyG (1 lanzo Dcho atrás- Jowke)", "EyG (Guitarra círculos - Jowke)",
   "EyG (Piano - Jowke)", "JyE (Diagonal + Giro en 6 Led + juego de pies + sombra Follow)", "AyA (CATs - Enrollo en 5)", "JyL (CATS - Freno Follow en 6 y yo sigo)", "Franceses (CATS - Freno Follow en 6 y ambos IZQ para péndulo)",
-  "MatySof (CATS - Freno en 6 con mano Drecha hombr Follow)"
+  "MatySof (CATS - Freno en 6 con mano Drecha hombr Follow)", "JyE (Diagonal + en 5 preparo para ponernos de espaldas)", "JyE (Sensual + rompo para sensual en 4)", "Chica Exorcista", "MOlino Follow", "Freno MOlino en 4", "Chica Exorcista", "MOlino Follow", "Freno MOlino en 4"
 ];
 
 const wordsCustom = [
@@ -64,7 +64,7 @@ const wordsCustom = [
   "JHERSY (en sombra hacer Rueda)", "JHERSY (En posición cerrada dos Ondas laterales rápidas)", "JHERSY (Mambo - tensión brazos 1o Izq + 5º Decha)", "ByL (MAD - Lanzo Decha)", "MyS (MAD - Cambio pos in 1)",
   "Illusion", "Bailar en el sitio - Mov pequeños", "EyG (Break 1 Jowke - Preparo en 4, en 5 estiro atrás Drcho)", "EyG (1 lanzo Dcho atrás- Jowke)", "EyG (Guitarra círculos - Jowke)",
   "EyG (Piano - Jowke)", "JyE (Diagonal + Giro en 6 Led + juego de pies + sombra Follow)", "AyA (CATs - Enrollo en 5)", "JyL (CATS - Freno Follow en 6 y yo sigo)", "Franceses (CATS - Freno Follow en 6 y ambos IZQ para péndulo)",
-  "MatySof (CATS - Freno en 6 con mano Drecha hombr Follow)"
+  "MatySof (CATS - Freno en 6 con mano Drecha hombr Follow)", "JyE (Diagonal + en 5 preparo para ponernos de espaldas)", "JyE (Sensual + rompo para sensual en 4)", "Chica Exorcista", "MOlino Follow", "Freno MOlino en 4", "Chica Exorcista", "MOlino Follow", "Freno MOlino en 4"
 ];
 
 const familyPrefixes = ["JHERSY", "CyC", "K_ELDE", "EyG", "CyT", "JyE", "DyY", "YyE", "VyA", "Stefy"];
@@ -277,4 +277,5 @@ document.addEventListener("touchend", e => {
     if (window.navigator.vibrate) window.navigator.vibrate(10);
   }
 }, {passive: true})
+
 
